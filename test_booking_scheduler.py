7aafd6a2587a43e86699ce8d6d076afa742ff2d5
donghee <do.choi@samsung.com>
2025-08-01 14:00:21 +0900

    step3

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -14,14 +14,7 @@ ON_THE_HOUR = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
 
 SUNDAY = "2021/03/28 17:00"
 MONDAY = "2024/06/03 17:00"
-
-class TestableBookingScheduler(BookingScheduler):
-    def __init__(self, capacity_per_hour, date_time: str):
-        super().__init__(capacity_per_hour)
-        self._date_time = date_time
-
-    def get_now(self):
-        return datetime.strptime(self._date_time, "%Y/%m/%d %H:%M")
+TIME_FORMAT = "%Y/%m/%d %H:%M"
 
 @pytest.fixture
 def customer(mocker):
@@ -110,15 +103,25 @@ def test_이메일이_있는_경우에는_이메일_발송(booking_scheduler_wit
 
     mail_mock.send_mail.assert_called_once()
 
-def test_현재날짜가_일요일인_경우_예약불가_예외처리(booking_scheduler, customer):
-    booking_scheduler = TestableBookingScheduler(CAPACITY_PER_HOUR, SUNDAY)
+def test_현재날짜가_일요일인_경우_예약불가_예외처리(mocker, customer):
+    mocker_get_now = mocker.patch(
+        "booking_scheduler.BookingScheduler.get_now",
+        return_value=datetime.strptime(SUNDAY, TIME_FORMAT)
+    )
+
+    booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
     schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, customer)
 
     with pytest.raises(ValueError):
         booking_scheduler.add_schedule(schedule)
 
-def test_현재날짜가_일요일이_아닌경우_예약가능(booking_scheduler, customer):
-    booking_scheduler = TestableBookingScheduler(CAPACITY_PER_HOUR, MONDAY)
+def test_현재날짜가_일요일이_아닌경우_예약가능(mocker, customer):
+    mock_get_now = mocker.patch(
+        'booking_scheduler.BookingScheduler.get_now',
+        return_value = datetime.strptime(MONDAY, TIME_FORMAT)
+    )
+
+    booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
     schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, customer)
 
     booking_scheduler.add_schedule(schedule)
